952d8ce8814fec11c9fb8b004563ce15981bae83
Hussein <Hus8sein.sal7loum@gmail.com>
2021-02-23 23:21:47 +0200

    update our menu-items to nnavigate with withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,7 +4,6 @@ import "./directory.styles.scss";
 import MenuItem from "../menu-item/menu-item.component";
 
 class Directory extends Component {
-    state: { sections: { title: string; imageUrl: string; id: number; }[]; };
     
     constructor() {
         super();
@@ -14,29 +13,37 @@ class Directory extends Component {
                 {
                     title: "hats",
                     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-                    id: 1
+                    id: 1,
+                    linkUrl: 'hats'
                 },
                 {
                     title: "jackets",
                     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-                    id: 2
+                    id: 2,
+                    linkUrl: ''
                 },
                 {
                     title: "sneakers",
                     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-                    id: 3
+                    id: 3,
+                    linkUrl: ''
+
                 },
                 {
                     title: "womens",
                     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
                     size: "large",
-                    id: 4
+                    id: 4,
+                    linkUrl: ''
+
                 },
                 {
                     title: "mens",
                     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
                     size: "large",
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
+
                 },                
             ]
         }
@@ -47,7 +54,7 @@ class Directory extends Component {
             <div className="directory-menu">
                 {
                     this.state.sections.map(
-                        menuItem => <MenuItem key={menuItem.id} title={menuItem.title} imageUrl={menuItem.imageUrl} size={menuItem.size}/>    
+                        ({id, ...otherSectionProps}) => <MenuItem key={id} { ...otherSectionProps } />    
                     )
                 }
             </div>
